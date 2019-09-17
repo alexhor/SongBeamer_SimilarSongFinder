7b339c7b87549dcb9359c724beb1726737587e20
Alexander Hornig <alexander@h-software.de>
2019-09-17 09:15:35 +0200

    begin saving results

--- Finder.py
+++ Finder.py
@@ -1,14 +1,66 @@
 from Song import Song
 from pathlib import Path
+import timeit, math, pickle
+
 
 class Finder:
     def __init__(self, song_dir):
+        self._song_dir = song_dir
+        # check if there are any old results to load
+        try:
+            with open("save.txt", "rb") as fp:
+                self._songs = pickle.load(fp)
+                pass
+        except:
+            self.reload_songs()
+
+    def reload_songs(self):
         # get all song files in the given directory
-        song_file_list = list(Path(song_dir).rglob("*.[[sS][nN][gG]"))
+        song_file_list = list(Path(self._song_dir).rglob("*.[[sS][nN][gG]"))
         # convet the files to song objects to hadle them
         self._songs = []
         for song_file in song_file_list:
-            self._songs.append(Song(song_file))
-            break
+            song = Song(song_file)
+            if (song.valid):
+                self._songs.append(song)
+            count += 1
+        self.calc_similarities()
+        self.save_results()
+
+    def calc_similarities(self):
+        # fetch all lines
+        all_lines_list = []
+        count = 0
+        for song in self._songs:
+            if (count >= 100): break
+            all_lines_list.extend(song._song_line_list)
+            count += 1
+
+        # prepare for calculations
+        required_calculations = len(all_lines_list) ** 2
+        calculations_done = 0
+        start = timeit.default_timer()
+        # start calculation
+        for line in all_lines_list:
+            for comparision_line in all_lines_list:
+                line.similarity(comparision_line)
+                calculations_done += 1
+                # show progress
+                if (calculations_done % 1000000 == 0):
+                    print(calculations_done / required_calculations * 100, "%")
+                    print('Time: ', math.floor(timeit.default_timer() - start))
+        # wrap up calculation
+        stop = timeit.default_timer()
+        print('Time: ', math.floor(stop - start))
+
+    def save_results(self):
+        with open("save.txt", "wb") as fp:
+            pickle.dump(self._songs, fp)
+
+    def __debug(self):
+        song = self._songs[0]
+        print(song)
+        song_line = song._song_line_list[0]
+        print(song_line._similarities)
 
 Finder("Songs")
--- Song.py
+++ Song.py
@@ -1,30 +1,41 @@
 from SongLine import Line
 import re
 
+
 class Song:
-    supported_verse_heading_list = ["Unbekannt", "Unbenannt", "Unknown", "Intro", "Vers", "Verse", \
-                                    "Strophe", "Pre-Bridge", "Bridge", "Misc", "Pre-Refrain", "Refrain", \
-                                    "Pre-Chorus", "Chorus", "Pre-Coda", "Zwischenspiel", "Instrumental", \
-                                    "Interlude", "Coda", "Ending", "Outro", "Teil", "Part", "Chor", "Solo"]
-    
+    supported_verse_heading_list = ["Unbekannt", "Unbenannt", "Unknown",
+                                    "Intro", "Vers", "Verse", "Strophe",
+                                    "Pre-Bridge", "Bridge", "Misc",
+                                    "Pre-Refrain", "Refrain", "Pre-Chorus",
+                                    "Chorus", "Pre-Coda", "Zwischenspiel",
+                                    "Instrumental", "Interlude", "Coda",
+                                    "Ending", "Outro", "Teil", "Part", "Chor",
+                                    "Solo"]
+
     def __init__(self, song_file):
+        self.valid = False
+        self._song_file = song_file
         self._song_line_list = []
         # read file line by line and convert them into song lines
-        with open(song_file) as file:
-            content = file.readlines()
-        self.read_lines(content)
-    
+        try:
+            with open(song_file, encoding='Windows-1252') as file:
+                content = file.readlines()
+            self.read_lines(content)
+            self.valid = True
+        except:
+            print("Error reading file", song_file._str)
+
     def read_lines(self, content):
         header_has_ended = False
         verse_ended_in_last_line = False
-        
+
         for line in content:
             line_is_songtext = True
             line = line.strip()
 
             # skip empty lines
             if(line == ""): continue
-            
+
             # check if we are still in the heading
             if(not header_has_ended and line[0] == "#"): continue
             else: header_has_ended = True
@@ -33,7 +44,7 @@ class Song:
             if(line == "--" or line == "---"):
                 verse_ended_in_last_line = True
                 continue
-            
+
             # check if this is a verse heading
             if(verse_ended_in_last_line):
                 # filter out custom marker
@@ -43,15 +54,16 @@ class Song:
                     split_line = line.split(' ', 1 )
                     # check for a verse heading
                     verse_heading = split_line[0]
-                    
+
                     if(len(split_line) == 2): heading_number = split_line[1]
                     else: heading_number = ""
-                    
-                    if((verse_heading in self.supported_verse_heading_list and (heading_number == "" or re.search("^[0-9][a-z]?$", heading_number))) \
-                       or \
-                       ((verse_heading == "Part" or verse_heading == "Teil") and re.search("^[A-Z]$", heading_number))):
+
+                    if((verse_heading in self.supported_verse_heading_list and
+                            (heading_number == "" or re.search("^[0-9][a-z]?$", heading_number))) or
+                            ((verse_heading == "Part" or verse_heading == "Teil") and
+                            re.search("^[A-Z]$", heading_number))):
                         line_is_songtext = False
-            
+
             # remove markers
             if(line_is_songtext):
                 if(line[0:3] == "#C " or line[0:3] == "#H "):
@@ -62,6 +74,9 @@ class Song:
             # check if the line has passed all tests and can be converted to a song line
             if(line_is_songtext):
                 self._song_line_list.append(Line(line, self))
-            
+
             # reset variables
             verse_ended_in_last_line = False
+
+    def __repr__(self):
+        return self._song_file.name
