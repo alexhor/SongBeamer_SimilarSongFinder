35d987b222168a2fc33acdd6f5c4346df0065827
Alexander Hornig <alexander@h-software.de>
2020-12-04 13:13:10 +0100

    Added song line parsing

--- SongSimilarityFinder/SongLoader.cs
+++ SongSimilarityFinder/SongLoader.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SongSimilarityFinder
@@ -88,9 +90,128 @@ namespace SongSimilarityFinder
         /// <param name="fileLocation">Location to load a song class from</param>
         protected void LoadSong(string fileLocation)
         {
-            System.Threading.Thread.Sleep(1000);
-            // TODO: load the song
-            Debug.WriteLine(fileLocation);
+            // Make sure the file exists
+            if (!File.Exists(fileLocation)) return;
+
+            State readerState = State.SongHeading;
+            bool readNextLine = true;
+            string currentLine = "";
+            using (StreamReader reader = new StreamReader(fileLocation, CodePagesEncodingProvider.Instance.GetEncoding(1252)))
+            {
+                // Loop the file line by line
+                while (!reader.EndOfStream)
+                {
+                    if (readNextLine) currentLine = reader.ReadLine();
+                    readNextLine = true;
+
+                    // skip empty lines
+                    if (currentLine == "") continue;
+
+                    if (State.SongHeading == readerState)
+                    {
+                        // Another heading line
+                        if ('#' == currentLine[0])
+                        {
+                            FilterSongInfo(currentLine);
+                            continue;
+                        }
+                        // The heading has ended and we can now read verses
+                        else
+                        {
+                            readerState = State.VerseEnded;
+                            readNextLine = false;
+                            continue;
+                        }
+                    }
+
+                    // Skip verse headings
+                    if (State.VerseEnded == readerState && IsVerseHeading(currentLine))
+                    {
+                        readerState = State.VerseHeading;
+                        continue;
+                    }
+
+                    if (State.VerseEnded == readerState || State.VerseHeading == readerState)
+                    {
+                        // Check if a verse has just ended
+                        if (currentLine.StartsWith("---") || currentLine.StartsWith("--"))
+                        {
+                            readerState = State.VerseEnded;
+                            continue;
+                        }
+
+                        // Ignore custom markers
+                        if (currentLine.StartsWith("$$M=")) continue;
+
+                        // At this point it is a valid song line
+
+                        // Strip markers
+                        if (currentLine.StartsWith("#C ") || currentLine.StartsWith("#H ")) currentLine = currentLine.Substring(3);
+                        else if (Regex.IsMatch(currentLine, "^##[0-9] ")) currentLine = currentLine.Substring(4);
+
+                        // Store the song line
+                        Debug.WriteLine(currentLine);
+                    }
+                }
+            }
+
+            Debug.WriteLine("####READING SONG DONE####");
         }
+
+        /// <summary>
+        /// Checks if the given line is a verse heading
+        /// </summary>
+        /// <param name="line">The line to check</param>
+        /// <returns>Whether the line is a verse heading</returns>
+        private bool IsVerseHeading(string line)
+        {
+            string[] splitLine = line.Split(' ');
+            if (2 < splitLine.Length) return false;
+
+            // Split the actual hading and a possibly following number or letter
+            string verseHeading = splitLine[0];
+            string headingNumber = 2 == splitLine.Length ? splitLine[1] : "";
+
+            if ((SupportedVerseHeadingList.Contains(verseHeading) && ("" == headingNumber || Regex.IsMatch(headingNumber, "^[0-9][0-9]?[a-z]?$")))
+                ||
+                (SupportedAlphaVerseHeadingList.Contains(verseHeading) && Regex.IsMatch(headingNumber, "^[A-Z]$")))
+            {
+                return true;
+            }
+
+            // No objections were raised, so this isn't a verse heading
+            return false;
+        }
+
+        protected void FilterSongInfo(string currentLine)
+        {
+            //throw new NotImplementedException();
+        }
+
+        protected enum State
+        {
+            SongHeading,
+            VerseEnded,
+            VerseHeading,
+        }
+
+        /// <summary>
+        /// All accepted verse headings that can be followed by [00-99] or [a-z]
+        /// </summary>
+        protected static string[] SupportedVerseHeadingList = new string[25]
+        {
+            "Unbekannt", "Unbenannt", "Unknown", "Intro", "Vers", "Verse", "Strophe",
+            "Pre-Bridge", "Bridge", "Misc", "Pre-Refrain", "Refrain", "Pre-Chorus",
+            "Chorus", "Pre-Coda", "Zwischenspiel", "Instrumental", "Interlude", "Coda",
+            "Ending", "Outro", "Teil", "Part", "Chor", "Solo"
+        };
+
+        /// <summary>
+        /// All accepted verse headings that have to be followed by [A-Z]
+        /// </summary>
+        protected static string[] SupportedAlphaVerseHeadingList = new string[2]
+        {
+            "Part", "Teil"
+        };
     }
 }
